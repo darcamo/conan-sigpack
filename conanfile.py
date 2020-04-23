7a4237fdf41d33556fa38d7433320a2c3abd9372
Darlan Cavalcante Moreira <darcamo@gmail.com>
2020-04-23 15:00:59 -0300

    Update sigpack version to 1.2.7 and bump armadillo dependency to >=9.800.3

--- conanfile.py
+++ conanfile.py
@@ -5,18 +5,20 @@ import shutil
 
 class SigpackConan(ConanFile):
     name = "sigpack"
-    version = "1.2.5"
+    version = "1.2.7"
     license = "Mozilla Public License 2.0"
     author = "Darlan Cavalcante Moreira (darcamo@gmail.com)"
     url = "https://github.com/darcamo/conan-recipes"
     description = "SigPack is a C++ signal processing library using the Armadillo library as a base."
     generators = "cmake"
     source_zip_filename = "sigpack-{0}.zip".format(version)
-    requires = ("armadillo/9.200.7@darcamo/stable",)
+    requires = ("armadillo/[>=9.800.3]@darcamo/stable", )
     homepage = "https://sourceforge.net/projects/sigpack/files/"
 
     def source(self):
-        tools.get("https://sourceforge.net/projects/sigpack/files/sigpack-{0}.zip/download".format(self.version))
+        tools.get(
+            "https://sourceforge.net/projects/sigpack/files/sigpack-{0}.zip/download"
+            .format(self.version))
         os.rename("sigpack-{}/".format(self.version), "sources")
 
     def package(self):
